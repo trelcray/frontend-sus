818498031e8ffa78daaee49d62125533ff8488a3
Trelcray <thaliszambarda@hotmail.com>
2022-08-04 19:19:48 -0300

    first commit

---
+++ src/screens/index.tsx
@@ -0,0 +1,9 @@
+export function Index(){
+    return (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+            <label className="font-bold mb-3">insira seu código do SUS!</label>
+            <input type="text" />
+        </div>
+
+    )
+}
